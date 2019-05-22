e77e31a6f3923d78278c1de1deae66c43505864c
AuroreMariscal <aurore@affinitic.be>
2019-05-22 13:47:50 +0200

    Add toggle js to details

--- cpskin/diazotheme/newDream/static/js/newdream.js
+++ cpskin/diazotheme/newDream/static/js/newdream.js
@@ -13,4 +13,10 @@ $( document ).ready(function() {
        $("#portal-globalnav a[tabindex]").click(function(){
         $("#hidden-search").hide('fast');
        });
+
+       $(".toggle-button").click(function(){
+         var toggle_button_id = this.id;
+         var toggle_block_id = toggle_button_id.replace("-button", "-block");
+         $("#" + toggle_block_id).toggleClass("active desactive");
+       });
 });
