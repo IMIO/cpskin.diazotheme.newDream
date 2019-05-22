960fefff32f6269e12fd6076d19a0c4155a79c7b
AuroreMariscal <aurore@affinitic.be>
2019-05-22 12:11:50 +0200

    Adapt template for event

--- cpskin/diazotheme/newDream/interfaces.py
+++ cpskin/diazotheme/newDream/interfaces.py
@@ -1,8 +1,9 @@
 from collective.iconifieddocumentactions.interfaces import IIconifiedDocumentActionsLayer
 from cpskin.core.interfaces import ICPSkinCoreLayer
+from plone.app.event.interfaces import IBrowserLayer
 
 
-class ICPSkinNewDreamLayer(ICPSkinCoreLayer, IIconifiedDocumentActionsLayer):
+class ICPSkinNewDreamLayer(ICPSkinCoreLayer, IIconifiedDocumentActionsLayer, IBrowserLayer):
     """
     Marker interface that defines a ZTK browser layer.
     """
