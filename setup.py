d1334fe4a59ab0636783ed0c5ee39cf134fd33e6
Laurent Lasudry <lasudry@gmail.com>
2022-01-20 11:42:03 +0100

    Back to development: 0.1.18

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Installer for the cpskin.diazotheme.newDream package."""
 
-version = '0.1.17'
+version = '0.1.18.dev0'
 
 from setuptools import find_packages
 from setuptools import setup
